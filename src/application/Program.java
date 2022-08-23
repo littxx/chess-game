05914d4f6b76b0e3405ff10cfcde61a8b6ed2a22
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-08-23 10:48:50 -0300

    first commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -11,8 +13,9 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		
 		System.out.println("program chess init");
+		Position pos = new Position(3, 5);
 		
-		
+		System.out.println(pos);
 		
 		
 		
