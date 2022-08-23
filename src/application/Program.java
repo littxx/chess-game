7d2a694a68c3bac044a49977d1a3c4be9f48ad01
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-08-23 10:38:14 -0300

    project

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("program chess init");
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
