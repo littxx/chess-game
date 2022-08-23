120440b98612f578f3f0536aa2e99c170917d48f
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-08-23 20:55:15 -0300

    ok

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
@@ -13,14 +14,9 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 		
-		System.out.println("program chess init");
-		Position pos = new Position(3, 5);
+		ChessMatch chessmatch = new ChessMatch();
+		UI.printBoard(chessmatch.getPieces());
 		
-		System.out.println(pos);
-		
-		Board board = new Board(8, 8);
-		
-		System.out.println(board);
 		
 		
 		
