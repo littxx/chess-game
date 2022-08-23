88ee84a1e3a26e4841729341f5a7f5c1b4d7faab
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-08-23 11:06:11 -0300

    board e peca

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -16,6 +17,10 @@ public class Program {
 		Position pos = new Position(3, 5);
 		
 		System.out.println(pos);
+		
+		Board board = new Board(8, 8);
+		
+		System.out.println(board);
 		
 		
 		
